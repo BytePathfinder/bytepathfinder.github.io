62fc3fdab33003327bf940636e41bbcf20f430c5
徐彪 <xubiao.java@outlook.com>
2024-09-16 14:36:46 +0800

    更新了文档站点的logo图像。
    在VitePress配置中，将站点logo从'/images/logo.png'更改为了'/images/favicon-32x32.png'，以反映新的品牌形象。

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   lastUpdated: true,
   themeConfig: {
     siteTitle: "XuBiao's Tech Docs",
-    logo: '/images/logo.png',
+    logo: '/images/favicon-32x32.png',
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
